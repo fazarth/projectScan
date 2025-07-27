7fb05b20d1ea8de1ac452c5fa6fa0385efa77b95
AbdMuafa13 <abdmuafa@gmail.com>
2025-07-27 15:32:42 +0700

    merge and fixing conflict

--- LoginMVCApp/LoginMVCApp/Models/Users.cs
+++ LoginMVCApp/LoginMVCApp/Models/Users.cs
@@ -19,16 +19,16 @@ namespace LoginMVCApp.Models
 
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Password minimal 8 karakter.")]
         [Required, DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         [NotMapped]
         [Required, DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Konfirmasi password tidak cocok.")]
-        public string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; } = string.Empty;
 
         [Required]
         [Column("role")]
-        public string Role { get; set; }
+        public string Role { get; set; } = string.Empty;
 
         [Column("line_id")]
         public long? LineId { get; set; }
@@ -41,7 +41,7 @@ namespace LoginMVCApp.Models
         [Column("created_at")]
         public DateTime CreatedAt { get; set; }
         [Column("createdby")]
-        public string CreatedBy { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
 
         public virtual Lines? Line { get; set; }
     }
--- LoginMVCApp/LoginMVCApp/ViewModels/EditUserViewModel.cs
+++ LoginMVCApp/LoginMVCApp/ViewModels/EditUserViewModel.cs
@@ -9,13 +9,13 @@ namespace LoginMVCApp.ViewModels
         public long Id { get; set; }
 
         [Required(ErrorMessage = "Nama lengkap wajib diisi")]
-        public string FullName { get; set; }
+        public string FullName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email wajib diisi")]
         [EmailAddress(ErrorMessage = "Format email tidak valid")]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
-        public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
 
         // Tidak wajib, hanya jika ingin ubah password
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Password minimal 8 karakter.")]
@@ -28,7 +28,7 @@ namespace LoginMVCApp.ViewModels
         public string? ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "Role wajib dipilih")]
-        public string Role { get; set; }
+        public string Role { get; set; } = string.Empty;
 
         [Column("line_id")]
         public long? LineId { get; set; }
